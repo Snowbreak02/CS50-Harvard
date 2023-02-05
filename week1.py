47429f1bf5234cbde5c5fdd1be5138a7dc85cfa2
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-02-05 10:44:08 +0000

    Sun, Feb 5, 2023, 6:44 PM +08:00

--- week1.py
+++ week1.py
@@ -1,7 +1,9 @@
+"""
 name = input ("What's your name? ")
 name = name.strip().title()
 print ("hello", name )
+"""
 x = int(input ("What is x? "))
 y = int(input ("What is y? "))
-z = x + y
+z = round(x / y, 2)
 print(z)
