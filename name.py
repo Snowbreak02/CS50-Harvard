82e740ab9b64917f674630d66b2320f6ef97e7db
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-02-05 11:05:38 +0000

    Sun, Feb 5, 2023, 7:05 PM +08:00

--- name.py
+++ name.py
@@ -1,10 +1,14 @@
+def main():
+    name = input ("What's your name? ")
+    name = name.strip().title()
+    greeting(name)
+
+
+
+
+
 def greeting(to="there"):
     print("hello", to)
 #to is defined as a value if no name is being called
 
-greeting()
-name = input ("What's your name? ")
-name = name.strip().title()
-print ("wassup", name )
-
-
+main()
