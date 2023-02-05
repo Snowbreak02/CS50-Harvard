73001f9b63ee76385ae49eb48e3a8f70c59f50ff
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-02-05 10:47:37 +0000

    Sun, Feb 5, 2023, 6:47 PM +08:00

---
+++ name.py
@@ -0,0 +1,10 @@
+def greeting():
+    print("wassup")
+
+
+
+name = input ("What's your name? ")
+name = name.strip().title()
+print ("hello", name )
+
+
--- week1.py
+++
@@ -1,9 +0,0 @@
-"""
-name = input ("What's your name? ")
-name = name.strip().title()
-print ("hello", name )
-"""
-x = int(input ("What is x? "))
-y = int(input ("What is y? "))
-z = round(x / y, 2)
-print(z)
