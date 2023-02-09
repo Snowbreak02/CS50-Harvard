d0f032aab264f754f66e9e389a748157218abf50
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-02-09 09:08:04 +0000

    Thu, Feb 9, 2023, 5:08 PM +08:00

---
+++ wk 2/dict.py
@@ -0,0 +1,24 @@
+"""
+students = {
+    "Jeremy" : "east" ,
+    "James" : "west",
+    "Hatim" : "north",
+    "Angus" : "south",
+}
+
+for student in students:
+    print(student, students[student], sep="| ")
+"""
+# [] means its going to be a list
+# {} means its going to be a 'dictionary' bcos u can link values tgt inside
+
+students = [
+    {"name": "Jeremy", "house": "Landed", "race": "chink"}
+    {"name": "James", "house": "HDB", "race": "chink"}
+    {"name": "Hatim", "house": "condo", "race": "indian"}
+    {"name": "Faeez", "house": "hdb", "race": None}
+
+]
+
+for student in students:
+    print(student["name"], students["house"], student["race"], sep="| ")
