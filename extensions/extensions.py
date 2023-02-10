80566cf2a81d0a2265867c9a0e345b282e8c059d
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-02-10 06:19:46 +0000

    Fri, Feb 10, 2023, 2:19 PM +08:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -1,3 +1,18 @@
 x = str(input("File name: "))
-x = x.islower()
-print(x)
+x = x.lower()
+if x.endswith(".gif"):
+    print("image/gif")
+elif x.endswith(".jpg"):
+    print("image/jpeg")
+elif x.endswith(".jpeg"):
+    print("image/jpeg")
+elif x.endswith(".png"):
+    print("image/png")
+elif x.endswith(".pdf"):
+    print("application/pdf")
+elif x.endswith(".txt"):
+    print("text/plain")
+elif x.endswith(".zip"):
+    print("application/zip")
+else:
+    print("application/octet-stream")
