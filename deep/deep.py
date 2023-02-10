9a6f4fe1f779cc528b8788946145a4244542e1d8
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-02-10 04:26:31 +0000

    Fri, Feb 10, 2023, 12:26 PM +08:00

--- deep/deep.py
+++ deep/deep.py
@@ -1,5 +1,5 @@
 x = str(input("What is the answer to the Great Question of Life, the Universe and Everything? "))
-if x == "42" or x== "forty-two", "forty two" ):
+if x == "42" or x == "forty-two" or x == "forty two":
     print ("Yes")
 else:
     print ("No")
