8d357e3e38a62cd6d1cd33cedc99a97f8223854a
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-02-10 10:43:54 +0000

    Fri, Feb 10, 2023, 6:43 PM +08:00

--- camel/camel.py
+++ camel/camel.py
@@ -1,10 +1,5 @@
 def main():
-    name = input ("What's your name? ")
-    name = name.strip().title()
-    greeting(name)
-
-def greeting(to="there"):
-    print("hello", to)
-#to is defined as a value if no name is being called
+    name = input ("camelCase: ")
+    
 
 main()
