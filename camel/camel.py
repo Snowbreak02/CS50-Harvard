ffe2b770aa76045d826daeddff7d264ee90da4db
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-02-10 10:59:36 +0000

    Fri, Feb 10, 2023, 6:59 PM +08:00

--- camel/camel.py
+++ camel/camel.py
@@ -1,6 +1,12 @@
 camel = input ("camelCase: ")
-print("snake_case)
+print("snake_case: ", end="")
 
+for letter in name:
+        if letter.isupper():
+            print("_" + letter.lower(), end = "")
+        else:
+            print(letter,end="")
+print()
 
 
 
