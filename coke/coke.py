a375a4ca32d09b5266e747d6fe66afbb69a2a299
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-02-13 03:20:01 +0000

    Mon, Feb 13, 2023, 11:20 AM +08:00

--- coke/coke.py
+++ coke/coke.py
@@ -2,5 +2,8 @@ amt_due = 50
 
 while amt_due >0:
     print("Amount due: ", amt_due)
-    inserted = input("Insert coin: ")
-    if inserted != 5 
+    inserted = int("Insert coin: ")
+    if inserted != (5, 10, 25):
+        inserted = input("Insert coin: ")
+else:
+    change_owed = amt_due - inserted
