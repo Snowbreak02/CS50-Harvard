c4b76cabd843dc06a057501e03098fd9bbd9eb36
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-02-14 07:06:49 +0000

    Tue, Feb 14, 2023, 3:06 PM +08:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -1,8 +1,8 @@
 x = str(input("Greeting: "))
 x = x.strip().title()
 
-for char in {"A", "E", "I", "O", "U","a", "e", "i", "o", "u"}:
+for char in ["A", "E", "I", "O", "U","a", "e", "i", "o", "u"]:
     if char in x:
-        new_x = x.replace(char,""+char)
+        x = x.replace(char,""+char)
 
-print(new_x)
+print(x)
