86f6091bd6ed4d63abf1775766d06e19958a0531
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-02-14 07:04:39 +0000

    Tue, Feb 14, 2023, 3:04 PM +08:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -1,5 +1,8 @@
 x = str(input("Greeting: "))
 x = x.strip().title()
 to_remov = {"A", "E", "I", "O", "U","a", "e", "i", "o", "u"}
-for char in to_remov(x):
-    new_x = 
+for char in to_remov():
+    if char in x:
+        new_x = x.replace(char,""+char)
+
+print(new_x)
