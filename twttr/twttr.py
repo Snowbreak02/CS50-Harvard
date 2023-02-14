c4b2b7ced1f847bb83ca3ab652ff6b7a313ef46d
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-02-14 07:08:16 +0000

    Tue, Feb 14, 2023, 3:08 PM +08:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -3,6 +3,6 @@ x = x.strip().title()
 
 for char in ["A", "E", "I", "O", "U","a", "e", "i", "o", "u"]:
     if char in x:
-        x = x.replace(char,""+char)
+        x = x.replace(char,"")
 
 print(x)
