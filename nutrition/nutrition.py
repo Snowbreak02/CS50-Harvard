d4718bcb6d69d18abcdad81a96dbd38c00a2cf2b
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-02-14 08:50:49 +0000

    Tue, Feb 14, 2023, 4:50 PM +08:00

--- nutrition/nutrition.py
+++ nutrition/nutrition.py
@@ -1,2 +1,23 @@
 food = input("Item: ")
 food = food.lowercase()
+
+match food:
+    case "apple":
+        print("Calories: 130")
+    case "avocado"|"cantaloupe"|"honeydew melon"|"pineapple"|"strawberries"|"tangerine":
+        print("Calories: 50")
+    case "banana":
+        print("Calories: 110")
+    case "grapefruit"|"nectarine"|"peach":
+        print("Calories: 60")
+    case "grapes"|"kiwifruit":
+        print("Calories: 90")
+    case "lemon":
+        print("Calories: 15")
+    case "lime":
+        print("Calories: 20")
+    case "orange"|"watermelon":
+        print("Calories: 80")
+    case "pear"|"sweet cherries":
+        print("Calories: 100")
+    
