f58b7813f76f90fe9b9a524664205c4c85e06d7e
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-02-14 07:01:51 +0000

    Tue, Feb 14, 2023, 3:01 PM +08:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -1,3 +1,5 @@
 x = str(input("Greeting: "))
 x = x.strip().title()
-to_remov = {"t": "T", "l": "L", "r": "R", "s": "S"}
+to_remov = {"A", "E", "I", "O", "U","a", "e", "i", "o", "u"}
+for char in to_remov(x):
+    new_x = 
