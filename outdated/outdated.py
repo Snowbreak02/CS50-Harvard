7c8dc8ac9b5eec51221a12d9ccce51f217dca964
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-02-15 09:47:30 +0000

    Wed, Feb 15, 2023, 5:47 PM +08:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -14,7 +14,7 @@ months = [
 ]
 
 while True:
-    in_date = input("Date: ").replace("/n","")
+    in_date = input("Date: ")
     try:
         month, date, year = in_date.split("/")
         if (int(month) <= 12 and int(month) >= 1 and int(date) <= 31 and int(date) >= 1):
