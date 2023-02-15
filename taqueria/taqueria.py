8b3048f9a57f36c58b0e813bbcf8a0e6e84168dc
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-02-15 06:20:50 +0000

    Wed, Feb 15, 2023, 2:20 PM +08:00

--- taqueria/taqueria.py
+++ taqueria/taqueria.py
@@ -1,5 +1,15 @@
-while True:
+try:
     item = input()
-    try:
-        
-    except EOFError:
+    menu = dict({"Baja Taco": 4.00,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00})
+    if item in menu:
+        print("Total: $)
+except EOFError:
+
