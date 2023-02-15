e22abf26721e1df9eb9bb959c7ac420167bf67e4
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-02-15 04:22:45 +0000

    Wed, Feb 15, 2023, 12:22 PM +08:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,14 +1,14 @@
 def main():
     x = get_frac("Fraction: ")
-    print(f"x is {}")
+    print(f"x is {x}")
 
 def get_frac(prompt):
     while True:
         try:
             fuel = input(prompt)
             numerator, denominator = fuel.split("/")
-            new_num = int(numer)
-            new_deno = int(deno)
+            new_num = int(numerator)
+            new_deno = int(denominator)
             f = new_num/new_deno
             if f <= 1:
                 break
@@ -16,7 +16,13 @@ def get_frac(prompt):
         except (ValueError, ZeroDivisionError):
             pass
 
+    perc = f * 100
 
-
+    if perc <= 1:
+        print("E")
+    elif perc >= 99:
+        print("F")
+    else:
+        return perc
 
 main()
