a73c77b52d13b95822fe9444201ab95da9500e5b
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-02-15 10:05:04 +0000

    Wed, Feb 15, 2023, 6:05 PM +08:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -17,7 +17,7 @@ while True:
     in_date = input("\r\nDate: ")
     try:
         month, date, year = in_date.split("/")
-        if (int(month) <= 12 and int(month) >= 1) and int(date) <= 31 and int(date) >= 1):
+        if (int(month) <= 12 and int(month) >= 1) and (int(date) <= 31 and int(date) >= 1):
             break
 
     except:
