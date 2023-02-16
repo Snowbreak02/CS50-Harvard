c86cc95d67a1b79fc1de6ac6ed4bbe3d091419b5
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-02-16 07:53:47 +0000

    Thu, Feb 16, 2023, 3:53 PM +08:00

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import json
 if len(sys.argv) != 2:
     sys.exit()
 
-response = requests.get("https://www.nike.com/sg/launch/t/womens-air-jordan-1-game-royal-and-varsity-maize" + sys.argv[1])
+response = requests.get("https://www.nike.com/sg/launch/t/womens-air-jordan-1-game-royal-and-varsity-maize")
 print(json.dumps(response.json(), indent=2))
 """
 o = response.json()
