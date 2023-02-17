49fa465b904a0db2e59415e179d75ef7599a7622
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-02-17 09:50:47 +0000

    Fri, Feb 17, 2023, 5:50 PM +08:00

--- game/game.py
+++ game/game.py
@@ -3,8 +3,6 @@ import random
 while True:
     try:
         lvl = int(input("Level: "))
-        print("value is", lvl)
-        break
-    
-    except (ValueError or lvl < 0):
+        
+    except ValueError:
         pass
