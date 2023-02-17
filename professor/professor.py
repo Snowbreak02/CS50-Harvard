68768daa4aa4cf725efa57b017e0711ea83627c6
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-02-17 10:53:32 +0000

    Fri, Feb 17, 2023, 6:53 PM +08:00

--- professor/professor.py
+++ professor/professor.py
@@ -14,9 +14,18 @@ def get_level():
         except:
             pass
 
-
 def generate_integer(level):
-    ...
+    if level == 1:
+        rng_x = random.randint(0,9)
+        rng_y = random.randint(0,9)
+    elif level == 2:
+        rng_x = random.randint(10,99)
+        rng_y = random.randint(10,99)
+    else:
+        rng_x = random.randint(100,999)
+        rng_x = random.randint(100,999)
+        
+    return rng_x,rng_y
 
 
 if __name__ == "__main__":
