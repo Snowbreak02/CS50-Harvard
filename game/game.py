338424c8dfbc005efce53eee87669cb4d360c956
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-02-17 10:18:29 +0000

    Fri, Feb 17, 2023, 6:18 PM +08:00

--- game/game.py
+++ game/game.py
@@ -4,15 +4,23 @@ while True:
     try:
         lvl = int(input("Level: "))
         if lvl > 0:
-            rng = random.randint(1,lvl)
-            while True:
-                guess = int(input("Guess: "))
-                if guess > rng:
-                    print("Too large!")
-                elif guess < rng:
-                    print("Too Small!")
-            except guess == rng:
-                print("Just right!")
+            break
+    except:
+        pass
+
+rng = random.randint(1,lvl)
+
+while True:
+    try:
+        guess = int(input("Guess: "))
+        if guess > 0:
+            if guess > rng:
+                print("Too large!")
+            elif guess < rng:
+                print("Too Small!")
+            else:
+                print("Just Right!")
                 break
+
     except:
         pass
