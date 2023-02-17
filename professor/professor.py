eb9044de9e9215d9e8d20afc4daa1189281f8c76
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-02-17 10:37:08 +0000

    Fri, Feb 17, 2023, 6:37 PM +08:00

--- professor/professor.py
+++ professor/professor.py
@@ -9,7 +9,7 @@ def get_level():
     while True:
         try:
             lvl = int(input("Level: "))
-            if lvl:
+            if lvl = [1,2,3]:
             break
     except:
         pass
--- test.py
+++ test.py
@@ -1,14 +1,7 @@
-import requests
-import sys
-import json
-
-if len(sys.argv) != 2:
-    sys.exit()
-
-response = requests.get("https://www.nike.com/sg/launch/t/womens-air-jordan-1-game-royal-and-varsity-maize")
-print(json.dumps(response.json(), indent=2))
-"""
-o = response.json()
-for result in o["results"]:
-    print(result["nikeSize"])
-"""
+while True:
+    try:
+        lvl = int(input("Level: "))
+        if lvl == ["1","2","3"]:
+            break
+    except:
+        pass
