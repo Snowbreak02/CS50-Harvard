d9008dfc8bbff54ec450c79c0fd648bef513cf71
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-02-17 10:55:09 +0000

    Fri, Feb 17, 2023, 6:55 PM +08:00

--- test.py
+++ test.py
@@ -1,7 +1,27 @@
-while True:
-    try:
-        lvl = int(input("Level: "))
-        if lvl in [1,2,3]:
-            break
-    except:
-        pass
+def generate_integer(level):
+    if level == "1" or "2" or "3":
+        count = 0
+        score = 0
+        while True:
+            while True:
+                try:
+                    x = random.randint(0,10)
+                    y = random.randint(0,10)
+                    print(f"{x} + {y} = ", end="")
+                    result = x + y
+                    answer = int(input())
+                    if answer == result:
+                        score += 1
+                        continue
+                    elif answer != result:
+                        count += 1
+                        print("EEE")
+                        continue
+                except ValueError:
+                    count += 1
+                    print("EEE")
+                    break
+            if count == 3:
+                    break
+
+    return score
