b8038cd98d4015694f9df67a16625aacccde9831
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-02-17 07:52:09 +0000

    Fri, Feb 17, 2023, 3:52 PM +08:00

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -4,10 +4,17 @@ names = {}
 
 while True:
     try:
-    in_name = input("Name: ")
-    
+        in_name = input("Name: ")
+        if in_name in names:
+            break
+        else:
+            in_name += names
 
+    except EOFError:
+        print()
+        break
 
+print({names})
 
 
 
