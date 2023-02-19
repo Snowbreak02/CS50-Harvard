11c9d450151bef1494cf753ef7b28ff603f11d91
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-02-19 08:55:25 +0000

    Sun, Feb 19, 2023, 4:55 PM +08:00

--- test_bank/bank.py
+++ test_bank/bank.py
@@ -10,16 +10,17 @@ else:
 """
 def main():
     x = str(input("Greeting: "))
-    x = x.strip().title()
-
+    greeting = x.strip().title()
+    final_val = value(greeting)
+    print("$",final_val)
 
 def value(greeting):
-    if "Hello" in x:
-    print("$0")
-    elif x.startswith("H"):
-        print("$20")
+    if "Hello" in greeting:
+        return 0
+    elif greeting.startswith("H"):
+        return 20
     else:
-        print("$100")
+        return 100
 
 
 if __name__ == "__main__":
