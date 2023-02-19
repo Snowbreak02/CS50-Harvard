1cd918474680eb4fd950b574bc921d0264852851
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-02-19 09:24:23 +0000

    Sun, Feb 19, 2023, 5:24 PM +08:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -1,21 +1,26 @@
 from plates import is_valid
 
 def main():
-    test_Hposition()
-    test_num()
-    test_punctuation()
+    test_length()
+    test_number_positions()
+    test_punctuations()
 
 def test_length():
-    assert value("Heys12") == "Valid"
-    assert value("hey12") == "Invalid"
-    assert value("Hello12") == "Invalid"
-    assert value("i") == "Invalid"
+    assert is_valid("Heys12") == "Valid"
+    assert is_valid("Hellothere12") == "Invalid"
+    assert is_valid("H") == "Invalid"
+    assert is_valid("Hi12") == "Invalid"
 
-def test_num():
-    assert value("1234") == 100
+def test_number_positions():
+    assert is_valid("12hey12") == "Invalid"
+    assert is_valid("he1y12") == "Invalid"
+    assert is_valid("h1ey12") == "Invalid"
 
-def test_punctuation():
-    assert value("%$&*!") == 100
+def test_punctuations():
+    assert is_valid("hey!12") == "Invalid"
+    assert is_valid("hey 12") == "Invalid"
+    assert is_valid("&hey12") == "Invalid"
+    assert is_valid("hey12.") == "Invalid"
 
 
 if __name__ == "__main__":
