9b71ff92f0172032d6ea78ca26952f4f611d512d
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-02-19 08:41:12 +0000

    Sun, Feb 19, 2023, 4:41 PM +08:00

--- test_twttr/test_twttr.py
+++ test_twttr/test_twttr.py
@@ -1,3 +1,21 @@
-import twttr
+from twttr import shorten
+
+def main():
+    test_upperNlower()
+    test_num()
+    test_punctuation()
+
+def test_upperNlower():
+    assert shorten("twitter") == "twttr"
+    assert shorten("TWITTER") == "TWTTR"
+    assert shorten("TwiTTeR") == "TwTTR"
+
+def test_num():
+    assert shorten("1234") == "1234"
+
+def test_punctuation():
+    assert shorten("%$&*!") == "%$&*!"
 
 
+if __name__ == "__main__":
+    main()
