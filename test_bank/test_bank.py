b394e915642c8ccabd9fcffe51300c13fa72b7a2
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-02-19 09:02:43 +0000

    Sun, Feb 19, 2023, 5:02 PM +08:00

---
+++ test_bank/test_bank.py
@@ -0,0 +1,22 @@
+from bank import value
+
+def main():
+    test_Hposition()
+    test_num()
+    test_punctuation()
+
+def test_Hposition():
+    assert value("Hey") == "$20"
+    assert value("cheers") == "$100"
+    assert value("Hello") == "$0"
+    assert value("hello") == "$0"
+
+def test_num():
+    assert value("1234") == "$100"
+
+def test_punctuation():
+    assert value("%$&*!") == "$100"
+
+
+if __name__ == "__main__":
+    main()
