643802ce79a44da25646d906a3b1d67c832cc1df
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-02-19 08:21:25 +0000

    Sun, Feb 19, 2023, 4:21 PM +08:00

--- test_twttr/twttr.py
+++ test_twttr/twttr.py
@@ -9,11 +9,17 @@ print(x)
 """
 
 #shortening it
+def main():
+    word = str(input("Greeting: "))
 
-x = str(input("Greeting: "))
-
-for char in ["A", "E", "I", "O", "U","a", "e", "i", "o", "u"]:
-    if char in x:
-        x = x.replace(char,"")
+def shorten(word):
+    for char in ["A", "E", "I", "O", "U","a", "e", "i", "o", "u"]:
+        if char in word:
+            word = word.replace(char,"")
+            return word
 
 print(x)
+
+
+if __name__ == "__main__":
+    main()
