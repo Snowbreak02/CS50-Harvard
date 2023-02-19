10aae126b70dbd3b56e2aca2aec1f60d849bc6c0
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-02-19 09:05:59 +0000

    Sun, Feb 19, 2023, 5:05 PM +08:00

--- test_bank/test_bank.py
+++ test_bank/test_bank.py
@@ -6,16 +6,16 @@ def main():
     test_punctuation()
 
 def test_Hposition():
-    assert value("Hey") == "$20"
-    assert value("cheers") == "$100"
-    assert value("Hello") == "$0"
-    assert value("hello") == "$0"
+    assert value("Hey") == 20
+    assert value("cheers") == 100
+    assert value("Hello") == 0
+    assert value("hello") == 0
 
 def test_num():
-    assert value("1234") == "$100"
+    assert value("1234") == 100
 
 def test_punctuation():
-    assert value("%$&*!") == "$100"
+    assert value("%$&*!") == 100
 
 
 if __name__ == "__main__":
