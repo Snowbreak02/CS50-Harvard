b96117806ac2d18867249469898f619f8981e728
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-02-19 09:45:38 +0000

    Sun, Feb 19, 2023, 5:45 PM +08:00

--- test_plates/plates.py
+++ test_plates/plates.py
@@ -18,14 +18,28 @@ def is_valid(s):
     if middle_char(s).isnumeric() == True:
         return False
 
+    length = len(s)
+
+    if not s.isalnum():
+        return False
+    if not s[0:2].isalpha():
+        return False
+    if not 2 <= length <= 6:
+        return False
+
     i = 0
-    while i < len(s):
-        if s[i].isalpha() == False:
+
+    while i < length - 1:
+        if not s[i].isalpha():
             if s[i] == "0":
                 return False
+            elif not s[i:].isdigit():
+                return False
             else:
-                break
-        i+=1
+                return True
+        i += 1
+
+    return True
 
     for char in s:
         if char in [".", "!", " ", "?"]:
