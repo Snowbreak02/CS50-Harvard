eb87088211aa4acf3e37d1176c6944c53037a045
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-02-20 11:02:01 +0000

    Mon, Feb 20, 2023, 7:02 PM +08:00

--- lines/lines.py
+++ lines/lines.py
@@ -4,9 +4,15 @@ import sys
 if len(sys.argv) == 2 :
     input = str(sys.argv[1]).lstrip()
     if input.endswith(".py"):
+        counter = 0
         try:
-            file = open(sys.argv[1], "r")
-            lines = file.readlines()
+            with open(input) as file:
+                for line in file:
+                    if not line.lstrip().startswith("#") and not line.isspace():
+                        counter += 1
+
+            print(counter)
+
         except FileNotFoundError:
             print("File does not exist")
             sys.exit(1)
