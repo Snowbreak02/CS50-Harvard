c8b4b5b8fb9dbf25c3a69829bf0a24de9375fac8
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-02-20 10:27:08 +0000

    Mon, Feb 20, 2023, 6:27 PM +08:00

--- lines/lines.py
+++ lines/lines.py
@@ -1,13 +1,16 @@
 import sys
 
-if len(sys.argv) == 1 :
-    input = str(sys.argv)
+if len(sys.argv) == 2 :
+    input = str(sys.argv[1])
     if input.endswith(".py"):
         try:
             print("Finding")
         except:
             print("File does not exist")
             sys.exit(1)
+    else:
+        print("Not a python file")
+        sys.exit(1)
 else:
     print("Too few command-line argument")
     sys.exit(1)
