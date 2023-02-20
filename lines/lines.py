dac5cd9858ea2105c13c44507128f223a9e18137
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-02-20 10:16:13 +0000

    Mon, Feb 20, 2023, 6:16 PM +08:00

--- lines/lines.py
+++ lines/lines.py
@@ -1,2 +1,11 @@
 import sys
 
+if len(sys.argv) == 2:
+    try:
+        input = float(sys.argv[1])
+    except:
+        print("Command-line argument is not a number")
+        sys.exit(1)
+else:
+    print("Missing command-line argument")
+    sys.exit(1)
