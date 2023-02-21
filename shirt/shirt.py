63b72835d3a0171fa16a95e584352973bdbfb7d7
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-02-21 12:38:34 +0000

    Tue, Feb 21, 2023, 8:38 PM +08:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -1,11 +1,20 @@
 import sys
-
+from PIL import Image,ImageOps
 
 if len(sys.argv) == 3 :
     input = str(sys.argv[1]).lstrip()
     if input.endswith(".csv"):
         try:
+            images = []
+            image_1 = Image.open(sys.argv[1])
+            res_image = ImageOps.fit(image_1, (400, 400))
+            images.append(res_image)
+            image_2 = Image.open(sys.argv[2])
+            res_image2 = ImageOps.fit(image_2, (500,500))
+            images.append(res_image2)
 
+        images[0].paste(images[1], box=(-50,-100), mask=images[1])
+        images[0].save("after.png")
 
 
         except FileNotFoundError:
