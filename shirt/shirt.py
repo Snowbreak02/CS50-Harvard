2894f597ae5bea21e29b33e4101a776cbbf1ea0b
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-02-21 12:44:31 +0000

    Tue, Feb 21, 2023, 8:44 PM +08:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -6,7 +6,7 @@ if len(sys.argv) > 3:
     print("Too many command-line arguments")
     sys.exit(1)
 
-elif sys.argv[1].lower().endswith(tuple(ext))  and sys.argv[2].lower().endswith(tuple(sources)) == False:
+elif sys.argv[1].lower().endswith(tuple(sources)) and sys.argv[2].lower().endswith(tuple(sources)) == False:
     print("File does not exist")
     sys.exit(1)
 
@@ -16,13 +16,13 @@ elif len(sys.argv) < 3:
 
 else:
     images = []
-            image_1 = Image.open(sys.argv[1])
-            res_image = ImageOps.fit(image_1, (400, 400))
-            images.append(res_image)
-            image_2 = Image.open(sys.argv[2])
-            res_image2 = ImageOps.fit(image_2, (500,500))
-            images.append(res_image2)
+    image_1 = Image.open(sys.argv[1])
+    res_image = ImageOps.fit(image_1, (400, 400))
+    images.append(res_image)
+    image_2 = Image.open(sys.argv[2])
+    res_image2 = ImageOps.fit(image_2, (500,500))
+    images.append(res_image2)
 
-        images[0].paste(images[1], box=(-50,-100), mask=images[1])
-        images[0].save("after.png")
+images[0].paste(images[1], box=(-50,-100), mask=images[1])
+images[0].save("after.png")
 
