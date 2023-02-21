933b3ca99d121db763d04c775c02994ad25a483b
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-02-21 12:43:31 +0000

    Tue, Feb 21, 2023, 8:43 PM +08:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -1,11 +1,21 @@
 import sys
 from PIL import Image,ImageOps
 
-if len(sys.argv) == 3 :
-    input = str(sys.argv[1]).lstrip()
-    if input.endswith(".csv"):
-        try:
-            images = []
+sources = [".jpg", ".jpeg" , ".png"]
+if len(sys.argv) > 3:
+    print("Too many command-line arguments")
+    sys.exit(1)
+
+elif sys.argv[1].lower().endswith(tuple(ext))  and sys.argv[2].lower().endswith(tuple(sources)) == False:
+    print("File does not exist")
+    sys.exit(1)
+
+elif len(sys.argv) < 3:
+    print("Too few command-line argument")
+    sys.exit(1)
+
+else:
+    images = []
             image_1 = Image.open(sys.argv[1])
             res_image = ImageOps.fit(image_1, (400, 400))
             images.append(res_image)
@@ -15,20 +25,4 @@ if len(sys.argv) == 3 :
 
         images[0].paste(images[1], box=(-50,-100), mask=images[1])
         images[0].save("after.png")
-
-
-        except FileNotFoundError:
-            print(f"Could not read {sys.argv[1]}")
-            sys.exit(1)
-
-    elif sys.argv[1].lower().endswith(tuple(ext))  and sys.argv[2].lower().endswith(tuple(ext)) == False:
-        sys.exit("File does not exist")
-        
-elif len(sys.argv) < 3:
-    print("Too few command-line argument")
-    sys.exit(1)
-
-else:
-    print("Too many command-line arguments")
-    sys.exit(1)
 
