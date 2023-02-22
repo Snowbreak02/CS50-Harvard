c45d55c821c77478ebfca6985559f046282cf519
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-02-22 06:55:09 +0000

    Wed, Feb 22, 2023, 2:55 PM +08:00

--- wk 7/validate.py
+++ wk 7/validate.py
@@ -3,9 +3,10 @@ import re
 email = input("What's your email? ").strip
 
 
-# .:any character except newline/ +: 1 ormore rep/ ?: 0 or 1 reps/ {m,n}:m-n reps
+# .:any character except newline/ +: 1 ormore rep to the thing to the left/ ?: 0 or 1 reps/ {m,n}:m-n reps
 # r allows some charcters to be some characters like \ to not be rec as a special char(if special char in the dict then bobian)
-if re.search(r"^.+@.+\.edu$^", email):
+# [^@]: any character except the @ sign
+if re.search(r"^[^@]+@[^@].+\.edu$", email):
     print("valid")
 else:
     print("invalid")
