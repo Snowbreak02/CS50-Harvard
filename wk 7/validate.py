8b426bdfb1189c81c2150b099bb62199f49f0943
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-02-22 06:25:08 +0000

    Wed, Feb 22, 2023, 2:25 PM +08:00

--- wk 7/validate.py
+++ wk 7/validate.py
@@ -2,7 +2,7 @@ import re
 
 email = input("What's your email? ").strip
 
-if re.search(".+@.+", email): #.:any character ex
+if re.search(".+@.+", email): #.:any character except newline/ +: 1 ormore rep/ ?: 0 or 1 reps/ {m,n}:m-n reps
     print("valid")
 else:
     print("invalid")
