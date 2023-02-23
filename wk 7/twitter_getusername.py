943b9bf8089ee871184e06ba1961355685a8cf35
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-02-23 04:28:14 +0000

    Thu, Feb 23, 2023, 12:28 PM +08:00

--- wk 7/twitter_getusername.py
+++ wk 7/twitter_getusername.py
@@ -3,9 +3,9 @@ import re
 
 url = input("URL: ").strip()
 
-matches = re.search(r"^(https://|www\.)twitter\.com/(.+)$", url, re.IGNORECASE) # "?:" means dont capture the stuff in this bracket
+matches = re.search(r"^(?:https://|www\.)twitter\.com/(.+)$", url, re.IGNORECASE) # "?:" means dont capture the stuff in this bracket
 
 if matches:
-    print(f"Username: ", matches.group(1))
+    print(f"Username:", matches.group(1))
 
 
