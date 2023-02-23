fa1fae8348cec70eabc70b290bdc623a60e23e2e
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-02-23 04:12:18 +0000

    Thu, Feb 23, 2023, 12:12 PM +08:00

--- wk 7/twitter_getusername.py
+++ wk 7/twitter_getusername.py
@@ -3,7 +3,7 @@ import re
 
 url = input("URL: ").strip()
 
-username = re.sub(r"^https?://twitter\.com/", "", url)
+username = re.sub(r"^https?://(www\.)?twitter\.com/", "", url)
 
 print(f"Username: )
 
