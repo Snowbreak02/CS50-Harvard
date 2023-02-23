c7e2cb74b96889355b4227205873e748e2498895
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-02-23 04:18:56 +0000

    Thu, Feb 23, 2023, 12:18 PM +08:00

--- wk 7/twitter_getusername.py
+++ wk 7/twitter_getusername.py
@@ -3,8 +3,9 @@ import re
 
 url = input("URL: ").strip()
 
-username = re.sub(r"^https?://(www\.)?twitter\.com/", "", url)
+matches = re.search(r"^https?://(www\.)?twitter\.com/(.+)$", url, re.IGNORECASE)
 
-print(f"Username: ")
+if matches:
+    print(f"Username: ", matches.group(2))
 
 
