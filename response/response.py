20854357bbc1a2cc6ce626e8134120683506907c
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-02-24 10:11:33 +0000

    Fri, Feb 24, 2023, 6:11 PM +08:00

--- response/response.py
+++ response/response.py
@@ -1,2 +1,10 @@
 from validator_collection import validators
 
+def main():
+    email_address = input("What's your email address? ")
+    try:
+        emailisvalid = validators.email(email_address)
+        print("Valid")
+    except:
+        print("Invalid")
+
