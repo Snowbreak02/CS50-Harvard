a694906d8ed16e34e4b0c91fb6c91198c89c1af4
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-02-24 08:20:42 +0000

    Fri, Feb 24, 2023, 4:20 PM +08:00

--- working/working.py
+++ working/working.py
@@ -7,7 +7,7 @@ def main():
 
 
 def convert(s):
-    iscorrectformat = re.search(r"^([0-9][0-2]*)(:([0-5][0-9]))? ([A-P]M) (to) ([0-9][0-2]*)(:([0-5][0-9]))? ([A-P]M)$", s)
+    iscorrectformat = re.search(r"^(([0-9][0-2]*):*([0-5][0-9])*) ([A-P]M) to (([0-9][0-2]*):*([0-5][0-9])*) ([A-P]M)$", s)
     if iscorrectformat:
         pieces = iscorrectformat.groups()
         if int(pieces[1]) > 12 or int(pieces[6]) > 12:
