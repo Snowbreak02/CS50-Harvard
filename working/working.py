667cb34c9ea7f1734aad0e8ceaa1614cca3e0c2d
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-02-24 08:13:08 +0000

    Fri, Feb 24, 2023, 4:13 PM +08:00

--- working/working.py
+++ working/working.py
@@ -7,11 +7,18 @@ def main():
 
 
 def convert(s):
-    if re.search(r"^[0-9][0-2]*(:([0-5][0-9]))? ([A-P]M) (to) [0-9][0-2]*(:([0-5][0-9]))? ([A-P]M)$", s):
-        if iscorrectformat:
-            pieces = iscorrectformat.groups()
-            if int(pieces[1])
+    iscorrectformat = re.search(r"^[0-9][0-2]*(:([0-5][0-9]))? ([A-P]M) (to) [0-9][0-2]*(:([0-5][0-9]))? ([A-P]M)$", s)
+    if iscorrectformat:
+        pieces = iscorrectformat.groups()
+        print(f"{pieces}")
 
+        """
+            if int(pieces[1]) > 12 or int(pieces[5]) > 12:
+                raise ValueError
+        return pieces
+    else:
+        raise ValueError
+            """
 
 
 
