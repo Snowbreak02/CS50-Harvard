39b21534f5bfe0c965bf8e44ddb21deccbebcee4
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-02-27 10:34:56 +0000

    Mon, Feb 27, 2023, 6:34 PM +08:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -4,14 +4,16 @@ from datetime import date
 
 
 def main():
-    p = inflect.engine()
+    p = inflect.engine
     get_bday = input("Date of Birth: ")
     get_day_diff = days_diff()
-    min_diff = p.number_to_words(int(days_diffent) * 24 * 60)
-    print f"{min_diff}"
+    min_diff = int(days_diffent) * 24 * 60
+    formatted_diff = p.number_to_words(int(min_diff), andword = "")
+    print (f"{formatted_diff}")
 
 
-...
+def days_diff(get_bday):
+    
 
 
 if __name__ == "__main__":
