191eed4a9d33bda9d841af35feb3be88c4fbc1b1
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-02-27 11:02:12 +0000

    Mon, Feb 27, 2023, 7:02 PM +08:00

---
+++ seasons/test_seasons.py
@@ -0,0 +1,20 @@
+from seasons import check_bday
+
+def main():
+    test_format()
+    test_range()
+
+def test_format():
+    assert validate(r"1.2.3.4") == True
+    assert validate(r"1.2.3") == False
+    assert validate(r"1.4") == False
+    assert validate(r"4") == False
+    assert validate(r"cat") == False
+
+
+def test_range():
+    assert validate(r"255.255.255.255") == True
+    assert validate(r"512.2.1.2") == False
+    assert validate(r"1.512.3.4") == False
+    assert validate(r"1.2.512.4") == False
+    assert validate(r"1.2.3.512") == False
