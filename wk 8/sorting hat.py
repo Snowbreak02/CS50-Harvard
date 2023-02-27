c0ffb9abeae1172dddec1f6fdf1d1cdf8af87383
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-02-27 09:25:45 +0000

    Mon, Feb 27, 2023, 5:25 PM +08:00

--- wk 8/sorting hat.py
+++ wk 8/sorting hat.py
@@ -4,8 +4,9 @@ class Hat:
 #dont need self as self is referring to a specific objects. There is only once copy of this variable and we want to use it throughout this entire class
     houses = ["Landed", "HDB", "Condo", "apartment"]
 
-    def sort(self, name):
-        print(name, "is gna live in a", random.choice(self.houses))
+    @classmethod
+    def sort(cls, name): # class wld conflict with class keyword
+        print(name, "is gna live in a", random.choice(cls.houses))
 
 
 hat = Hat()
