8dbce585da6bdcb51b2cc317e6b4a80794361f2b
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-02-27 09:24:01 +0000

    Mon, Feb 27, 2023, 5:24 PM +08:00

--- wk 8/sorting hat.py
+++ wk 8/sorting hat.py
@@ -1,8 +1,8 @@
 import random
 
 class Hat:
-    def __init__(self):
-        self.houses = ["Landed", "HDB", "Condo", "apartment"]
+#dont need self as self is referring to a specific objects. There is only once copy of this variable and we want to use it throughout this entire class
+    houses = ["Landed", "HDB", "Condo", "apartment"]
 
     def sort(self, name):
         print(name, "is gna live in a", random.choice(self.houses))
