488539dbfa70723c3d25335d7a499f26df425135
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-02-27 10:10:52 +0000

    Mon, Feb 27, 2023, 6:10 PM +08:00

---
+++ wk 8/operator_overload.py
@@ -0,0 +1,24 @@
+class Vault:
+    def __init__(self, dollars=0, cents=0, penny=0):
+        self.dollars = dollars
+        self.cents = cents
+        self.penny = penny
+
+    def __str__(self):
+        return f"{self.dollars} dollars, {self.cents} cents, {self.penny} penny
+
+    def __add__(self, other): #overloading the + sign below, combining both of them tgt
+        dollars = self.dollars + other.dollars
+        cents = self.cents + other.cents
+        penny = self.penny +other.penny
+
+
+
+jeremy = Vault(100, 50, 20)
+print(jeremy)
+
+shawn = Vault(1000, 500, 25)
+print(shawn)
+
+total = jeremy + shawn # as + is an add operator, it will call the __add__ overload operator above
+print(total)
