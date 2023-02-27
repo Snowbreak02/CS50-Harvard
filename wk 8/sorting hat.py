4bdeaf8e279d51d58b0600e0f69d57580796270f
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-02-27 09:19:53 +0000

    Mon, Feb 27, 2023, 5:19 PM +08:00

--- wk 8/sorting hat.py
+++ wk 8/sorting hat.py
@@ -1,9 +1,11 @@
+import random
+
 class Hat:
     def __init__(self):
         self.houses = ["Landed", "HDB", "Condo", "apartment"]
 
     def sort(self, name):
-        print(name, "is in", random.choice(self.houses))
+        print(name, "is gna live in a", random.choice(self.houses))
 
 
 hat = Hat()
