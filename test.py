00b7e5db990698209562843dcd54a246a3a3fbee
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-02-27 09:11:26 +0000

    Mon, Feb 27, 2023, 5:11 PM +08:00

--- test.py
+++ test.py
@@ -1,27 +1,2 @@
-def generate_integer(level):
-    if level == "1" or "2" or "3":
-        count = 0
-        score = 0
-        while True:
-            while True:
-                try:
-                    x = random.randint(0,10)
-                    y = random.randint(0,10)
-                    print(f"{x} + {y} = ", end="")
-                    result = x + y
-                    answer = int(input())
-                    if answer == result:
-                        score += 1
-                        continue
-                    elif answer != result:
-                        count += 1
-                        print("EEE")
-                        continue
-                except ValueError:
-                    count += 1
-                    print("EEE")
-                    break
-            if count == 3:
-                    break
-
-    return score
+value = input("what: ")
+print(value.strip('a'))
