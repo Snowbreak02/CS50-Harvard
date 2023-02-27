ac0b7bade99508a226fcb1eb3e162b976b00f7e5
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-02-27 09:48:32 +0000

    Mon, Feb 27, 2023, 5:48 PM +08:00

--- wk 8/class in class.py
+++ wk 8/class in class.py
@@ -1,4 +1,21 @@
 class Wizard:
-    def __init(self):
+    def __init__(self):
         if not name:
             raise ValueError("Invalid name")
+        self.name = name
+
+    ...
+
+
+class Student(Wizard): #tells py to inherit the Wizard class, which is the super class
+    def __init__(self, name, house):
+        self.house = house
+
+    ...
+
+
+class Professor(Wizard):
+    def __init__(self, name, subject):
+        self.subject = subject
+
+    ...
