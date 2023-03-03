70c4286e17fa6f3e727e95e68fa045ccec978b75
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-03-03 08:57:00 +0000

    Fri, Mar 3, 2023, 4:57 PM +08:00

--- project.py/test_project.py
+++ project.py/test_project.py
@@ -14,11 +14,8 @@ def main():
 )
 
 
-def test_Hposition(monkeypatch):
-    inputs = iter(['Pavol', 'Kutaj'])
-    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
-    result = get_player_names()
-    assert result == "'Pavol' and 'Kutaj' will be playing against each other!⚔️"
+def test_same_name(a,b,expected):
+    assert demo.add(a,b) == expected
 """
 def test_num():
     assert value("1234") == 100
