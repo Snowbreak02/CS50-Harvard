b19a44d44d28893f93b6e46293bb9198ee273a80
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-03-03 08:55:19 +0000

    Fri, Mar 3, 2023, 4:55 PM +08:00

--- project.py/test_project.py
+++ project.py/test_project.py
@@ -2,6 +2,17 @@ from project import get_player_names
 
 def main():
     test_same_name()
+    """
+    test_num()
+    test_punctuation()
+"""
+@pytest.mark.parametrize(
+    'a,b,expected', [
+        (Jeremy, Justin, "'Jeremy' and 'Justin' will be playing against each other!⚔️")
+        (Jeremy, Thomas, "'Jeremy' and 'Thomas' will be playing against each other!⚔️")
+    ]
+)
+
 
 def test_Hposition(monkeypatch):
     inputs = iter(['Pavol', 'Kutaj'])
