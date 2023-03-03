b116c09a882af514045e10a83863b120561ca405
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-03-03 08:26:34 +0000

    Fri, Mar 3, 2023, 4:26 PM +08:00

--- project.py/project.py
+++ project.py/project.py
@@ -43,15 +43,21 @@ MAX_VAL = 70
 DICE_FACE = 6
 
 def get_player_names():
-    while True:
-        p1_name = input("\nName of first player: ").strip()
-        p2_name = input("\nName of second player: ").strip()
-    assert p1_name != p2_name
-        break
-    except if userinput.isdigit():
-        print("Names must not contain numbers")
-    except AssertionError:
-        print("The two names cant be the same. Re-enter two unique names!")
+while True:
+        p1_name = input("\nName of first player: ")
+        if p1_name.isalpha():
+            break
+        else:
+            print("Please use only letters, try again")
+
+while True:
+        p2_name = input("\nName of second player: ")
+        if p2_name.isalpha() and p2_name != p1_name:
+            break
+        if p2_name == p1_name:
+            print("Please do not use the same names!")
+        else:
+            print("Please use only letters, try again")
 
     print(f"\n'{p1_name}' and '{p2_name}' will be playing against each other!⚔️")
     return p1_name, p2_name
