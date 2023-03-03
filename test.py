a61b6cec537260122905bacd88de9f03f365449a
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-03-03 08:09:11 +0000

    Fri, Mar 3, 2023, 4:09 PM +08:00

--- test.py
+++ test.py
@@ -2,14 +2,8 @@ p1_name = input("\nName of first player: ").strip()
 p2_name = input("\nName of second player: ").strip()
 
 
-while True:
-    p1_name = input("\nName of first player: ").strip()
-    p2_name = input("\nName of second player: ").strip()
+if p1_name != p2_name:
     try:
-
-    except:
-        if p1_name == p2_name:
-            print("Enter names again")
-        break
+        
 
 print(f"\n'{p1_name}' and '{p2_name}' will be playing against each other!⚔️")
