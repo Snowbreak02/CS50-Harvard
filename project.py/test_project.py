781845b47cff1daf6b8277995432a090b56e62bb
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-03-03 08:37:52 +0000

    Fri, Mar 3, 2023, 4:37 PM +08:00

--- project.py/test_project.py
+++ project.py/test_project.py
@@ -2,21 +2,19 @@ from project import get_player_names
 
 def main():
     test_same_name()
-    test_num()
-    test_punctuation()
 
 def test_Hposition(monkeypatch):
     inputs = iter(['Pavol', 'Kutaj'])
     monkeypatch.setattr('builtins.input', lambda _: next(inputs))
-    result = say_hello()
-    assert result == "Hello Pavol Kutaj"
-
+    result = get_player_names()
+    assert result == "'Pavol' and 'Kutaj' will be playing against each other!⚔️"
+"""
 def test_num():
     assert value("1234") == 100
 
 def test_punctuation():
     assert value("%$&*!") == 100
-
+"""
 
 if __name__ == "__main__":
     main()
