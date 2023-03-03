9d49afce9f7a18bec865223fd30f970ea1e16036
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-03-03 07:52:50 +0000

    Fri, Mar 3, 2023, 3:52 PM +08:00

--- test.py
+++ test.py
@@ -1,12 +1,13 @@
 while True:
-    p1_name = input("\nName of first player: ").strip()
-    p2_name = input("\nName of second player: ").strip()
+    try:
+        p1_name = input("\nName of first player: ").strip()
+        p2_name = input("\nName of second player: ").strip()
 assert p1_name != p2_name
     break
-except if userinput.isdigit():
-    print("Names must not contain numbers")
-except AssertionError:
-    print("The two names cant be the same. Re-enter two unique names!")
+    except if p1_name.isdigit() or p2_name.isdigit():
+        print("Names must not contain numbers")
+    except AssertionError:
+        print("The two names cant be the same. Re-enter two unique names!")
 
 print(f"\n'{p1_name}' and '{p2_name}' will be playing against each other!⚔️")
 break
