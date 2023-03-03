cad7988c4f6abe9a0cfac0067a82b70915272b07
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-03-03 06:17:36 +0000

    Fri, Mar 3, 2023, 2:17 PM +08:00

--- test.py
+++ test.py
@@ -1,2 +1,2 @@
 print("Hi there, Welcome to my very first game of Snakes and Ladders, created using Python!")
-print("This is a 2 player game where each player will take turns rolling the dice and advancing. May the luckiest player win!")
+print("This is a 2 player game where each player will take turns rolling the dice and advancing. May the luckiest player win!🏆")
