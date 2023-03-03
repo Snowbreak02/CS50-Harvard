7362881f4d1f40e16e6e2dc5295d425602ebb70a
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-03-03 08:29:29 +0000

    Fri, Mar 3, 2023, 4:29 PM +08:00

--- project.py/test_project.py
+++ project.py/test_project.py
@@ -1,2 +1,22 @@
 from project import get_player_names
 
+def main():
+    test_Hposition()
+    test_num()
+    test_punctuation()
+
+def test_Hposition():
+    assert value("Hey") == 20
+    assert value("cheers") == 100
+    assert value("Hello") == 0
+    assert value("hello") == 0
+
+def test_num():
+    assert value("1234") == 100
+
+def test_punctuation():
+    assert value("%$&*!") == 100
+
+
+if __name__ == "__main__":
+    main()
