4cbfa40e83a49782a1d6ee186a4a5d23d2153b1a
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-03-03 08:11:56 +0000

    Fri, Mar 3, 2023, 4:11 PM +08:00

--- test.py
+++ test.py
@@ -1,9 +1,12 @@
 p1_name = input("\nName of first player: ").strip()
 p2_name = input("\nName of second player: ").strip()
+while True:
+        p1_name = input("\nName of first player: ").strip()
+        p2_name = input("\nName of second player: ").strip()
+        if p1_name.isalpha() and p2_name.isalpha():
+            break
+        else:
+            print("Please use only letters, try again")
 
-
-if p1_name != p2_name:
-    try:
-        
 
 print(f"\n'{p1_name}' and '{p2_name}' will be playing against each other!⚔️")
