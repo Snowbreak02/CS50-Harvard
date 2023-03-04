295575ffa81a5d8646df56300c0d2f31cc541e17
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-03-04 07:06:25 +0000

    Sat, Mar 4, 2023, 3:06 PM +08:00

--- test.py
+++ test.py
@@ -2,19 +2,19 @@
 def main():
     p1_name = input("\nName of first player: ").strip()
     p2_name = input("\nName of second player: ").strip()
-    p1_named, p2_named = check_player_names()
+    p1_named, p2_named = check_player_names(p1_name, p2_name)
 
 def check_player_names(p1_name, p2_name):
     while True:
             if p1_name.isalpha():
-                p1_name = p1_named
+                p1_named = p1_name
                 break
             else:
                 print("Please use only letters, try again")
 
     while True:
             if p2_name.isalpha() and p2_name != p1_name:
-                p2_name = p2_named
+                p2_named = p2_name
                 break
             if p2_name == p1_name:
                 print("Please do not use the same names!")
