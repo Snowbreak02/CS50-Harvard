14a1a84e60a7b0c3a2da20f32e4c17e9eadb1afb
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-03-04 09:01:13 +0000

    Sat, Mar 4, 2023, 5:01 PM +08:00

--- project.py/test_project.py
+++ project.py/test_project.py
@@ -2,8 +2,7 @@ from project import get_player_names
 import pytest
 from unittest.mock import patch
 
-def main():
-    test_parameters = [
+test_parameters = [
                     ("Alan\nJack\n", "'Alan' and 'Jack' will be playing against each other!⚔️\n"),
                     ("Mary\nMary\n", "'Mary' and 'Mary' will be playing against each other!⚔️\n"),
                     ("12345\nJenny\n", "'12345' and 'Jenny' will be playing against each other!⚔️\n")
@@ -19,6 +18,3 @@ def test_get_player_names(user_input, expected_output, capfd):
     # Capturing user output
     captured = capfd.readouterr()
     assert captured.out == expected_output
-
-if __name__ == "__main__":
-    main()
