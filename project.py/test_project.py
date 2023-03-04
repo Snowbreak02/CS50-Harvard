346037faf4c952a6907b393614ef7fafd3183978
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-03-04 12:18:12 +0000

    Sat, Mar 4, 2023, 8:18 PM +08:00

--- project.py/test_project.py
+++ project.py/test_project.py
@@ -1,12 +1,18 @@
-from project import get_player_names import welc_msg() import get_dice_value() import bitten() import ladder() import snake_ladder_actions() import check_win()
+from project import get_player_names
+from project import welc_msg 
+from project import get_dice_value
+from project import bitten
+from project import ladder
+from project import snake_ladder_actions
+from project import check_win
 import pytest
 from unittest.mock import patch
-
-test_parameters = [
-                    ("Alan\nJack\n", "\n'Alan' and 'Jack' will be playing against each other!⚔️\n"),
-                    ("Mary\nMary\n", "\n'Mary' and 'Mary' will be playing against each other!⚔️\n"),
-                    ("12345\nJenny\n", "\n'12345' and 'Jenny' will be playing against each other!⚔️\n")
-                  ]
+def main():
+    test_parameters = [
+                        ("Alan\nJack\n", "\n'Alan' and 'Jack' will be playing against each other!⚔️\n"),
+                        ("Mary\nMary\n", "\n'Mary' and 'Mary' will be playing against each other!⚔️\n"),
+                        ("12345\nJenny\n", "\n'12345' and 'Jenny' will be playing against each other!⚔️\n")
+                    ]
 
 @pytest.mark.parametrize("user_input, expected_output", test_parameters)
 def test_get_player_names(user_input, expected_output, capfd):
@@ -18,3 +24,6 @@ def test_get_player_names(user_input, expected_output, capfd):
     # Capturing user output
     captured = capfd.readouterr()
     assert captured.out == expected_output
+
+if __name__ == "__main__":
+    main()
