a7dedf670e3138de69b9445649038a686cf8c6ad
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-03-04 07:00:20 +0000

    Sat, Mar 4, 2023, 3:00 PM +08:00

--- test.py
+++ test.py
@@ -1,19 +1,23 @@
-def get_player_names():
+p1_name = input("\nName of first player: ").strip()
+p2_name = input("\nName of second player: ").strip()
+p1_named, p2_named = check_player_names()
+
+def check_player_names(p1_name, p2_name):
     while True:
-            p1_name = input("\nName of first player: ")
             if p1_name.isalpha():
+                p1_name = p1_named
                 break
             else:
                 print("Please use only letters, try again")
 
     while True:
-            p2_name = input("\nName of second player: ")
             if p2_name.isalpha() and p2_name != p1_name:
+                p2_name = p2_named
                 break
             if p2_name == p1_name:
                 print("Please do not use the same names!")
             else:
                 print("Please use only letters, try again")
 
-    print(f"\n'{p1_name}' and '{p2_name}' will be playing against each other!⚔️")
-    return p1_name, p2_name
+    print(f"\n'{p1_named}' and '{p2_named}' will be playing against each other!⚔️")
+    return p1_named, p2_named
