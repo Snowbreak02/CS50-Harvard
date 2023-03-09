ef3025ee8c61e3453f8f77652d4dde75b7baada3
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-03-09 10:01:29 +0000

    Thu, Mar 9, 2023, 6:01 PM +08:00

--- CS50AI/Problems/wk_0/degrees.py
+++ CS50AI/Problems/wk_0/degrees.py
@@ -119,7 +119,7 @@ while True:
     explored.add(node.state)
 
     # Add neighbors to frontier
-    for action, state in self.neighbors(node.state):
+    for action, state in neighbors_for_person(node.state):
         if not frontier.contains_state(state) and state not in self.explored:
             child = Node(state=state, parent=node, action=action)
             frontier.add(child)
