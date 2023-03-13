8581085e7f8acb33f24c1b3bce93494121294fcf
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-03-13 07:41:40 +0000

    Mon, Mar 13, 2023, 3:41 PM +08:00

--- For Fun/IU.py
+++ For Fun/IU.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 # send an HTTP request to the album page
-url = 'https://kprofiles.com/iu-discography/'
+url = 'https://karchives.com/iu-discography/'
 response = requests.get(url).text
 
 # create a BeautifulSoup object to parse the HTML
