3a95c8850e35b6778f35533ad63e395314139bc8
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-03-13 06:03:09 +0000

    Mon, Mar 13, 2023, 2:03 PM +08:00

--- For Fun/IU.py
+++ For Fun/IU.py
@@ -3,10 +3,10 @@ from bs4 import BeautifulSoup
 
 # send an HTTP request to the album page
 url = 'https://www.melon.com/album/detail.htm?albumId=10827816'
-response = requests.get(url)
+response = requests.get(url).text
 
 # create a BeautifulSoup object to parse the HTML
-soup = BeautifulSoup(response.text, 'html.parser')
+soup = BeautifulSoup(response, 'lxml')
 
 # find the album name tag and extract its text
 album_name = soup.find('div', class_= 'service_list_song d_song_list').text.strip()
