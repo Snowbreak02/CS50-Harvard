988373bf4ee1dadb3be1635ee17e4f7206635915
Snowbreak02 <101542210+Snowbreak02@users.noreply.github.com>
2023-03-30 09:14:36 +0000

    Thu, Mar 30, 2023, 5:14 PM +08:00

--- hello_world_django/tasks/views.py
+++ hello_world_django/tasks/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 
-tasks = ["yes","no","hi"]
+tasks = ["eat","sleep","play"]
 
 
 def index(request):
     return render(request, "tasks/index.html", {
-    "tasks": tasks
+        "tasks": tasks
     })
